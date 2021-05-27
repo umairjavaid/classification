88b5e5135184414ec2a938ca824912a501c2e18c
umairjavaid <bscs13024@itu.edu.pk>
2021-05-28 03:38:49 +0500

    Update person_category_controller.py

--- person_category_controller.py
+++ person_category_controller.py
@@ -10,16 +10,7 @@ def read_img(img_path):
     img = cv2.imread(img_path)
     return img
 
-def get_person_tag(img_left, img_top, img_width, img_height, img_path=None, img=None):
-  #place check img_path and img both can not be empty
-  if(img_path == None and img == None):
-    raise Exception("img_path and img can not be empty")
-    quit()
-  if(img == None and img_path != None):
-    img = read_img(img_path)
-  if(img == None):
-    raise Exception("img can not be none")
-  
+def get_person_tag(img_left, img_top, img_width, img_height, img):
   patch = img[img_top:img_top+img_height,img_left:img_left+img_width,:]
   cat = get_person_cat(patch)
   return cat
