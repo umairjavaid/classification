cf5f688269928c4decd0bac1a52d292f4d64bce1
umairjavaid <bscs13024@itu.edu.pk>
2021-06-22 11:48:26 +0500

    Update amp-trainer.py

--- amp-trainer.py
+++ amp-trainer.py
@@ -1,1 +1,355 @@
+from torchvision import models
+import torch
+import torchvision
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision import transforms, datasets
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import torch.nn as nn
+from torch.utils.model_zoo import load_url
+import torch.nn.functional as F
+import os
+from efficientnet_pytorch import EfficientNet
+import torch.onnx
 
+
+def initialize_weights(modules, init_mode):
+    for m in modules:
+        if isinstance(m, nn.Conv2d):
+            if init_mode == 'he':
+                nn.init.kaiming_normal_(m.weight, mode='fan_out',
+                                        nonlinearity='relu')
+            elif init_mode == 'xavier':
+                nn.init.xavier_uniform_(m.weight.data)
+            else:
+                raise ValueError('Invalid init_mode {}'.format(init_mode))
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm2d):
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.Linear):
+            nn.init.normal_(m.weight, 0, 0.01)
+            nn.init.constant_(m.bias, 0)
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None,
+                 base_width=64):
+        super(Bottleneck, self).__init__()
+        width = int(planes * (base_width / 64.))
+        self.conv1 = nn.Conv2d(inplanes, width, 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(width)
+        self.conv2 = nn.Conv2d(width, width, 3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(width)
+        self.conv3 = nn.Conv2d(width, planes * self.expansion, 1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNetCam(nn.Module):
+    def __init__(self, block, layers, num_classes=2, large_feature_map=True):
+        super(ResNetCam, self).__init__()
+
+        stride_l3 = 1 if large_feature_map else 2
+        self.inplanes = 64
+
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2,
+                                padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=stride_l3)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=1)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        initialize_weights(self.modules(), init_mode='xavier')
+
+    def forward(self, x, labels=None, return_cam=False):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        pre_logit = self.avgpool(x)
+        pre_logit = pre_logit.reshape(pre_logit.size(0), -1)
+        logits = self.fc(pre_logit)
+
+        if return_cam:
+            feature_map = x.detach().clone()
+            cam_weights = self.fc.weight[labels]
+            cams = (cam_weights.view(*feature_map.shape[:2], 1, 1) *
+                    feature_map).mean(1, keepdim=False)
+            return cams
+        return {'logits': logits}
+
+    def _make_layer(self, block, planes, blocks, stride):
+        layers = self._layer(block, planes, blocks, stride)
+        return nn.Sequential(*layers)
+
+    def _layer(self, block, planes, blocks, stride):
+        downsample = get_downsampling_layer(self.inplanes, block, planes, stride)
+        layers = [block(self.inplanes, planes, stride, downsample)]
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+        return layers
+
+
+def load_pretrained_model(model, name):
+    if name == "resnet50":
+        state_dict = load_url('https://download.pytorch.org/models/resnet50-19c8e357.pth', progress=True)
+        state_dict = remove_layer(state_dict, 'fc')   
+    else: 
+        print("the name of the model you entered does not exsist!")
+        exit()
+    model.load_state_dict(state_dict, strict=False)
+    return model
+
+def get_downsampling_layer(inplanes, block, planes, stride):
+    outplanes = planes * block.expansion
+    if stride == 1 and inplanes == outplanes:
+        return
+    else:
+        return nn.Sequential(
+            nn.Conv2d(inplanes, outplanes, 1, stride, bias=False),
+            nn.BatchNorm2d(outplanes),
+        )
+
+def remove_layer(state_dict, keyword):
+    keys = [key for key in state_dict.keys()]
+    for key in keys:
+        if keyword in key:
+            state_dict.pop(key)
+    return state_dict
+
+data_transforms = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.Resize((150,150)),
+        #transforms.RandomResizedCrop(224),
+        transforms.RandomAffine(15),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+data_dir = "/content/dataset/"
+dataset = datasets.ImageFolder(data_dir,transform= data_transforms)
+train_dataset, test_dataset = torch.utils.data.random_split(dataset,[700,174], generator=torch.Generator().manual_seed(0))
+train_loader = torch.utils.data.DataLoader(
+    train_dataset,
+    batch_size=32,
+    num_workers=4,
+    shuffle=True
+)
+test_loader = torch.utils.data.DataLoader(
+    test_dataset,
+    batch_size=32,
+    num_workers=4,
+    shuffle=True
+)
+dataloaders_dict = {'train': train_loader, 'val':test_loader}
+
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=32):
+    since = time.time()
+    device = "cuda:0"
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    global inputs_, labels_
+    #image_info.next(model,save_cams =True)
+    
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                #inputs_.data = inputs.clone()
+                labels = labels.to(device)
+                #for sigmoid related loss
+                #labels = labels.unsqueeze(1)
+                #labels = labels.float()
+                #print("inputs.size(): ", inputs.size())
+                #print("labels.size(): ", labels.size())
+                #labels_.data = labels.clone()
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                # forward
+                # track history if only in train
+                #print(model(inputs))
+                #print("*****************************************************************************")
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    output = model(inputs)
+                    #output = output['logits']
+                    loss = criterion(output, labels)
+                    _, preds = torch.max(output, 1)
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=0):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+
+    def get_attention(self, input, target):
+        prob = F.softmax(input, dim=-1)
+        prob = prob[range(target.shape[0]), target]
+        prob = 1 - prob
+        prob = prob ** self.gamma
+        return prob
+
+    def get_celoss(self, input, target):
+        ce_loss = F.log_softmax(input, dim=1)
+        ce_loss = -ce_loss[range(target.shape[0]), target]
+        return ce_loss
+
+    def forward(self, input, target):
+        attn = self.get_attention(input, target)
+        ce_loss = self.get_celoss(input, target)
+        loss = self.alpha * ce_loss * attn
+        return loss.mean()
+
+floss1 = FocalLoss(alpha=0.25, gamma=2)
+
+class CEFL(nn.Module):
+    def __init__(self, gamma=1):
+        super(CEFL, self).__init__()
+        self.gamma = gamma
+
+    def get_prob(self, input, target):
+        prob = F.softmax(input, dim=-1)
+        prob = prob[range(target.shape[0]), target]
+        return prob
+
+    def get_attention(self, input, target):
+        prob = self.get_prob(input, target)
+        prob = 1 - prob
+        prob = prob ** self.gamma
+        return prob
+
+    def get_celoss(self, input, target):
+        ce_loss = F.log_softmax(input, dim=1)
+        ce_loss = -ce_loss[range(target.shape[0]), target]
+        return ce_loss
+
+    def forward(self, input, target):
+        attn = self.get_attention(input, target)
+        ce_loss = self.get_celoss(input, target)
+        prob = self.get_prob(input, target)
+        loss = (1-prob)*ce_loss + prob*attn*ce_loss
+        return loss.mean()
+
+cefl = CEFL(gamma=1)
+
+def tune_model(model, loss=cefl):
+    device = "cuda:0"
+    model.to(device)
+    params_to_update = model.parameters()
+    optimizer_ft = optim.SGD(params_to_update, lr=0.000227913316, momentum=0.9)
+    criterion = loss
+    model, hist = train_model(model, dataloaders_dict, criterion, optimizer_ft, num_epochs=40)
+    return model
+
+model = EfficientNet.from_pretrained('efficientnet-b0',num_classes=2)
+print(model)
+
+#model = ResNetCam(Bottleneck, [3, 4, 6, 3])
+#model = load_pretrained_model(model, "resnet50")
+model = tune_model(model)
+#saved_model_path = "/content/saved_models"
+#os.chdir(saved_model_path)
+torch.save(model.state_dict(), "/content/saved_models/maskClassification-efficientNet-CEFL.pt")
